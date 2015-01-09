05477773cf641728b25d4d61dc755085de23c94e
PF <p_fire@126.com>
2015-01-09 09:43:11 -0500

    Upgrading the period; add a clear program to clear the record ...

---
+++ CLEAR.py
@@ -0,0 +1,14 @@
+f2 = open("wordswithLevelandTime.txt", "r")
+data = f2.readlines()
+words = []
+for i in range(5014):
+	mean = data[i]
+	mean = mean.split()
+	
+	word = mean[0]
+	words.append(word)
+f3 = open("wordswithLevelandTime.txt", "w")
+for i in range(5014):
+	f3.write(words[i])
+	f3.write(" 0 0")
+	f3.write("\n")
--- recitingWords.py
+++ recitingWords.py
@@ -2,6 +2,7 @@
 # This is a word-reciting software. It is very succinct.
 import wx
 import random
+import time
 #words = []
 mean_word = {}
 time_word = {}
@@ -16,13 +17,10 @@ def readF():
 
 	f2 = open("wordswithLevelandTime.txt", "r")
 	data = f2.readlines()
-	#print vocabulary,"\n"*50, data
-	# f3 = open("words.txt", "r+")
-	# words = f3.readlines()
 	
 	global mean_word, time_word, lv_word, words
 	
-	for i in range(5013):
+	for i in range(5014):
 		mean = vocabulary[i]
 		mean = mean.split()
 		
@@ -38,14 +36,12 @@ def readF():
 		
 		levelandTime = data[i]
 		levelandTime = levelandTime.split()
-		#print "\n"*50,levelandTime
 		
 		level = levelandTime[1]
 		time = levelandTime[2]
-		#print "\n"*50,level,time
 		
 		time_word[str(word)] = float(time)
-		lv_word[str(word)] = float(level)
+		lv_word[str(word)] = int(level)
 	
 		# ----------------------------------------------------
 
@@ -55,10 +51,11 @@ def writeF():
 	f3 = open("wordswithLevelandTime.txt", "w")
 	global lv_word,time_word,words
 	
-	for i in range(5013):
+	for i in range(5014):
 		f3.write(words[i])
 		f3.write(" " + str(lv_word[words[i]]) + " " + str(time_word[words[i]]))
 		f3.write("\n")
+	
 	
 
 class WordsRecitingFrame(wx.Frame):
@@ -67,29 +64,31 @@ class WordsRecitingFrame(wx.Frame):
 		global time_word, mean_word
 		self.word_using = max(time_word, key=time_word.get)
 	
-	
-		self.begin = wx.Panel(self)
+		self.currentTime = time.time()
 		self.panel = wx.Panel(self)
-		self.panel.Show(False)
 		
 # --------------------------------------First Panel-----------------------------------------
+			
 		
-		self.test = wx.StaticText(self.begin, label="lalala")
+		# self.begin = wx.Panel(self)
+		
+		# self.panel.Show(False)
+	
+		# self.test = wx.StaticText(self.begin, label="lalala")
 
-		self.background1 = wx.Image("Background1.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-		self.background1 = wx.StaticBitmap(self.begin, wx.ID_ANY, self.background1, pos=(100, 100))
+		# self.background1 = wx.Image("Background1.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+		# self.background1Bitmap = wx.StaticBitmap(self.begin, wx.ID_ANY, self.background1, pos=(100, 100))
 		
-		self.SATlogo = wx.Image("Background1.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-		self.SATlogoBit = wx.StaticBitmap(self.begin, wx.ID_ANY, self.SATlogo, pos=(100, 100))	
+		# self.SATlogo = wx.Image("Background1.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+		# self.SATlogoBitmap = wx.StaticBitmap(self.begin, wx.ID_ANY, self.SATlogo, pos=(100, 100))	
 
 		
 # --------------------------------------First Panel-----------------------------------------
 
 		self.know = wx.Button(self.panel, label="Know", pos=(0,200))
-		self.dontknow = wx.Button(self.panel, label="Don't know", pos=(200,200))
+		self.dontknow = wx.Button(self.panel, label="I'm Noob", pos=(200,200))
 		self.correct = wx.Button(self.panel, label="Correct", pos=(0,200))
 		self.incorrect = wx.Button(self.panel, label="Incorrect", pos=(200,200))
-		self.close = wx.Button(self.panel, label="Close", pos=(300,0))
 		self.next = wx.Button(self.panel, label="Next", pos=(0,200))
 	
 		self.word = wx.StaticText(self.panel, label=self.word_using, pos=(0,0))
@@ -100,10 +99,8 @@ class WordsRecitingFrame(wx.Frame):
 		self.panel.Bind(wx.EVT_CHAR_HOOK, self.OnKey)
 		self.correct.Bind(wx.EVT_BUTTON, self.OnCorrect)
 		self.incorrect.Bind(wx.EVT_BUTTON, self.OnIncorrect)
-		self.close.Bind(wx.EVT_BUTTON, self.OnClose)
 		self.next.Bind(wx.EVT_BUTTON, self.OnIncorrect)
-		#self.panel.Bind(wx.EVT_CHAR_HOOK, self.OnKey)
-	
+		self.Bind(wx.EVT_CLOSE, self.OnClose)
 		self.mean.Show(False)
 		self.correct.Show(False)
 		self.incorrect.Show(False)
@@ -111,8 +108,6 @@ class WordsRecitingFrame(wx.Frame):
 
 	def OnKey(self, e):
 		key = e.GetKeyCode()
-		#print key,self.correct.IsShown(),self.know.IsShown(), self.next.IsShown()
-		#wx.Sleep(0.5)
 	
 		if(key == 67 and self.correct.IsShown() == True): self.OnCorrect(e)
 		if(key == 73 and self.correct.IsShown() == True): self.OnIncorrect(e)
@@ -139,13 +134,13 @@ class WordsRecitingFrame(wx.Frame):
 		
 	def OnCorrect(self,e):
 		global time_word,mean_word,time_lv
-		period = 3
+		period = time.time() - self.currentTime
+		self.currentTime = time.time()
 		for word in time_word: 
 			if(time_word[word] < 0): time_word[word] += period
 		
 		lv_word[self.word_using] += 1
 		time_word[self.word_using] = time_lv[lv_word[self.word_using]]
-		#print time_word
 		self.next.Show(False)
 		self.correct.Show(False)
 		self.incorrect.Show(False)
@@ -154,24 +149,23 @@ class WordsRecitingFrame(wx.Frame):
 		self.word.SetLabel(self.word_using)
 		self.mean.SetLabel(mean_word[self.word_using])
 		
-		
 		self.mean.Show(False)
 		self.know.Show(True)
 		self.dontknow.Show(True)
 		
 	def OnIncorrect(self, e):
 		global time_word,mean_word,time_lv
-		period = 3
+		period = time.time() - self.currentTime
+		self.currentTime = time.time()
 		for word in time_word: 
 			if(time_word[word] < 0): time_word[word] += period
 		
 		lv_word[self.word_using] = 0
 		time_word[self.word_using] = time_lv[0]
-		#print time_word
+
 		self.next.Show(False)
 		self.correct.Show(False)
 		self.incorrect.Show(False)
-		
 		
 		self.word_using = max(time_word, key=time_word.get)
 		self.word.SetLabel(self.word_using)
